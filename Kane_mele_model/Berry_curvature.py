8952d52014957ed0b3cfbf9cd88b2e954842b073
JYNi16 <49049752+JYNi16@users.noreply.github.com>
2023-08-14 19:36:27 +0800

    Add files via upload

--- Kane_mele_model/Berry_curvature.py
+++ Kane_mele_model/Berry_curvature.py
@@ -29,8 +29,8 @@ s0 = np.array([[1,0],[0,1]])
 J1 = 1
 D = 0.1
 
-dkp = 0.000001  #用于求导的微元
-numk = 201 # 用于计算贝里曲率的kx与ky的k点密度
+dkp = 0.000001  #
+numk = 201 # the density in the k-points to calculate Berry Curvature
 v = 0 
 c = 1
 
@@ -39,14 +39,14 @@ def H(k):
     kx, ky = k[0], k[1]
     #print("kx is:", kx, "ky is:", ky)
     gk = np.exp(1.j*k.dot(a1)) + np.exp(1.j*k.dot(a2)) + np.exp(1.j*k.dot(a3))
-    H[0,0] = 3*J1
+    H[0,0] = -3*J1
     H[0,1] = J1 * gk
     H[1,0] = J1 * gk.conj()
-    H[1,1] = 3*J1
+    H[1,1] = -3*J1
     
     #add DM term 
     dk = np.sin(k.dot(d1)) + np.sin(k.dot(d2)) + np.sin(k.dot(d3))
-    Hd = D*dk * sz
+    Hd = D*dk*sz
     
     return H+Hd
 
@@ -57,7 +57,7 @@ def dHy(k):
     k2 = k - np.array([0,dkp])
     return (H(k) - H(k2))/dkp
 
-#按顺序匹配对应的本征值和本征矢
+#sorting the Eigenstates according to the Eigenvalues
 def ewH(k):
     e,w=np.linalg.eigh(H(k))
     w0 = w[:, np.argsort(np.real(e))[0]]
@@ -65,7 +65,7 @@ def ewH(k):
     e = np.sort(np.real(e))
     return w0,w1,e[0],e[1]
 
-#<v|dH/dk|c> v,c 取0对应价带波函数，1对应导带波函数
+#<v|dH/dk|c> v,c 
 def vcdHx(v,k,c):
     dhc = np.dot(dHx(k), ewH(k)[c])
     vdhc = np.dot(ewH(k)[v].conj(),dhc)
@@ -77,36 +77,45 @@ def vcdHy(v,k,c):
     return vdhc  
 
 def Omega(k):
-    print("this is ok")
     return 1.j *  (vcdHx(v,k,c) * vcdHy(c,k,v) - vcdHy(v,k,c) * vcdHx(c,k,v))/(ewH(k)[2]-ewH(k)[3])**2
 
 
 def plot_berry():
 
-    xx = np.linspace(-1.45*np.pi,1.45*np.pi,numk)
-    yy = np.linspace(-1.45*np.pi,1.45*np.pi,numk)
+    xx = np.linspace(-4,4,numk)
+    yy = np.linspace(-4,4,numk)
     Z= np.zeros((numk,numk))
     X,Y = np.meshgrid(xx,yy)
+    
+    print("X.shape is:", X.shape)
     
     for i in range(numk):
         for j in range(numk):
             k = np.array([xx[i],yy[j]])
-            print("Omega(k) is:",)
             Z[i][j]=np.real(Omega(k))
 
     #fig = plt.figure()
     #ax = fig.add_subplot(1, 1, 1, projection='3d')
     #surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
     #        linewidth=0, antialiased=False)
-    #ax.set_xlim3d(0.0, num)
-    #ax.set_ylim3d(0.0, num)
+    #ax.set_xlim3d(0.0, 200)
+    #ax.set_ylim3d(0.0, 200)
     #ax.set_zlim3d(-0.90, 0.90)
     #plt.show()
-    
-    fig = plt.figure(figsize=(5,5))
-    plt.contourf(X,Y,Z,10,alpha=0.1,cmap=cm.RdYlGn)
-    C=plt.contour(X,Y,Z,10,colors='black',linewidths=0.1)
+    font = {'family': "Times New Roman", "weight":"normal", "size":20,}
+    fig = plt.figure(figsize=(10,8))
+    #plt.scatter(xx, yy, c=Z)
+    plt.pcolormesh(X,Y,Z,  cmap='coolwarm', shading='gouraud')
+    #C=plt.contour(X,Y,Z,10,colors='black',linewidths=0.1)
     #plt.clabel(C, inline=True,fontsize=10)
+    plt.colorbar()
+    plt.xlim(-4,4)
+    plt.ylim(-4,4)
+    plt.xlabel(r"$k_{x}$", font)
+    plt.ylabel(r"$k_{y}$", font)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.savefig("Berry_curvature.png", dpi=800)
     plt.show()
 
 
