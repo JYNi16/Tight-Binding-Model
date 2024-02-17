546cd4b4903bfb0685514beb29c95939520fe472
JYNI16 <16210190007@fudan.edu.cn>
2024-02-17 19:51:49 +0800

    rashba part

--- 3D_square/Hamiltonian.py
+++ 3D_square/Hamiltonian.py
@@ -28,25 +28,27 @@ class Square_monolayer():
 
 class Square_3D():
     
-    def __init__(self, t1=-1, soc_r=-0.5, soc_i = -0):
+    def __init__(self, e0=0, tab=-1, tc = -1,  soc_r=-0.5):
         self.H =np.zeros((2,2), dtype=complex)
         
-        self.t1 = t1
+        self.tab = tab
+        self.tc = tc
+        self.e0 = e0
         
         #Rashba SOC 
         self.soc_r = soc_r
         
-        #Intrinic SOC
-        self.soc_i = soc_i
     
     def model(self, k):
         kx, ky, kz = k 
         
-        self.H[0,0] = self.t1 * 2 * (np.cos(ky) + np.cos(kx) + np.cos(kz))
-        self.H[1,1] = self.t1 * 2 * (np.cos(ky) + np.cos(kx) + np.cos(kz))
+        self.H[0,0] = self.tab*2*(np.cos(ky) + np.cos(kx)) + self.tc*2*(np.cos(kz)) 
+        self.H[1,1] = self.tab*2*(np.cos(ky) + np.cos(kx)) + self.tc*2*(np.cos(kz))
         
-        self.H[0,1]  = 2 * self.soc_r * (1.j * np.sin(kx) + np.sin(ky))
-        self.H[1,0]  = 2 * self.soc_r *(-1.j * np.sin(kx) + np.sin(ky))
+        #Rashba SOC
+        L_r = 2*self.soc_r*(1.j*np.sin(kx) + np.sin(ky))
         
+        self.H[0,1] = L_r
+        self.H[1,0] = L_r.conjugate()
         
         return self.H
