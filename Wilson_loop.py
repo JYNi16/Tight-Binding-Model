4636a5388e1964c9b1d52af076342e21edc041c3
Curry <49049752+JYNi16@users.noreply.github.com>
2024-05-25 15:30:28 +0800

    Add files via upload

--- Wilson_loop.py
+++ Wilson_loop.py
@@ -8,12 +8,15 @@ Created on Fri May 24 18:37:20 2024
 import numpy as np 
 from BHZ_model import BHZ 
 from band_ini import config as cf
+import matplotlib.pyplot as plt
 
-Ham = BHZ(-2.1)
+Ham = BHZ(-2.5)
 
 A=1
 B=1
 delta=1
+
+Nband = 1
 
 def test_model(k):
     kx, ky = k
@@ -26,39 +29,61 @@ def H(k):
 
 def ewH(k):
     e,w=np.linalg.eigh(H(k))
-    e,w=np.linalg.eigh(H(k))
-    w0 = w[:, np.argsort(np.real(e))[0]]
-    w1 = w[:, np.argsort(np.real(e))[1]]
-    e = np.sort(np.real(e))
-        
-    return w0, e[0]
+    
+    s_idx = np.argsort(e)
+    s_e = e[s_idx]
+    s_w = w[:, s_idx]       
+    return s_w
+
+def Vmn(w1, w2, Ds): 
+    for i in range(Nband):
+        for j in range(Nband):
+            Ds[i,j] = np.dot(w1[:,i].conj().T, w2[:,j])
+    return Ds
+            
 
 def Wcc():
     xx = np.linspace(-np.pi, np.pi, 161)
     yy = np.linspace(-np.pi, np.pi, 101)
-    for kx in xx: 
-        Ds = np.zeros((1,1))
-        vD = 1
+    k_sita = []
+    for i in range(len(xx)): 
+        Ds = np.zeros((Nband, Nband), dtype=complex)
+        vD = np.ones((Nband, Nband), dtype=complex)
         for b in range(len(yy)-1):
             ky = yy[b]
-            w, e = ewH([kx, ky])
+            VN = ewH([xx[i], ky])
             
-            VN = w 
             ky2 = yy[b+1]
-            w, e = ewH([kx, ky2])
-            VM = w
+            VM = ewH([xx[i], ky2])
             
-            Ds[0,0] = np.dot(VN.conj().T, VM)
-            
-            vD *= Ds
-            
-            print("vD is:",Ds)
+            Ds = Vmn(VN, VM, Ds) 
+            vD = np.dot(vD, Ds)
         
-        k_sita = np.real(1.j*(np.log(np.linalg.eigh(vD)[0]))/(2*np.pi))
-        
-        print("k_sita is:", k_sita)
+        k_sita.append(np.real(1.j*(np.log(np.linalg.eig(vD)[0])))/(2*np.pi))
     
+    return xx, k_sita[::-1]
+
+
+def plot_wcc():
+    xx, k_sita = Wcc()
+    
+    font = {'family': "Times New Roman", "weight":"normal", "size":24,}
+    fig = plt.figure(figsize=(10,8))
+    
+    plt.scatter(xx, k_sita, c= "none", s= 75, marker = "o", edgecolors="r")
+    #plt.scatter(xx, yy, c=Z)
+    #C=plt.contour(X,Y,Z,10,colors='black',linewidths=0.1)
+    plt.yticks(fontproperties='Times New Roman', fontsize = 20)
+    plt.xticks(fontproperties='Times New Roman', fontsize = 20)
+    plt.xlim(-np.pi,np.pi)
+    plt.ylim(-0.5,0.5)
+    plt.xlabel(r"$k_{x}$", font)
+    plt.ylabel(r"$Wcc$", font)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.show()
             
 
 
-Wcc()
+if __name__=="__main__":
+    plot_wcc()
